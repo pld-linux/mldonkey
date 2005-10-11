ccc6260b40338e40e42195088abd3aa2cdac2286
Elan Ruusamäe <glen@pld-linux.org>
2005-10-11 08:28:18 +0000

    - remove sysconfig/mldonkey, it's for initscript, and this script doesn't use any variable set from there
    Changed files:
        mldonkey.sh -> 1.3

--- mldonkey.sh
+++ mldonkey.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-
-. /etc/sysconfig/mldonkey
-
 set -e
 
 if [ ! -d ~/.mldonkey ]; then
-	 echo "Creating $HOME/.mldonkey"
-	 mkdir $HOME/.mldonkey
-   fi
+	echo "Creating $HOME/.mldonkey"
+	mkdir $HOME/.mldonkey
+fi
 
 cd ~/.mldonkey && exec /usr/bin/mlnetd
